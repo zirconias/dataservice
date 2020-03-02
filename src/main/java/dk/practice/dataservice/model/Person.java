42e15d77434412fde1576c1b7c2955046da9f3cf
zirconias <xir@DESKTOP-85D6U89.localdomain>
2020-03-02 21:29:23 +0100

    add person resource

---
+++ src/main/java/dk/practice/dataservice/model/Person.java
@@ -0,0 +1,25 @@
+package dk.practice.dataservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Person
+ */
+@Data
+@NoArgsConstructor
+@Entity
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private String firstName;
+	private String lastName;
+}
---
+++ src/main/java/dk/practice/dataservice/repository/PersonRepository.java
@@ -0,0 +1,19 @@
+package dk.practice.dataservice.repository;
+
+/**
+ * PersonRepository
+ */
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import dk.practice.dataservice.model.Person;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+	List<Person> findByLastName(@Param("name") String name);
+
+}
